28fcf14ea3db58a1c2953c9a47a3c51a02d46ec1
Mario Kawakita <mario.jhk@gmail.com>
2024-09-24 18:26:47 +0000

    first commit

---
+++ app/(test)/index.tsx
@@ -0,0 +1,5 @@
+import { ThemedView } from "@/components/ThemedView";
+
+export default function TestScreen() {
+  return <ThemedView>Hello World!</ThemedView>;
+}
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,48 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
+
+// Use: Primary color for highlights, buttons, and important UI elements.
+const pikachuYellow = "#ffcb05";
+
+// Use: Accent color for interactive elements like buttons, icons, and notifications.
+const pokeballRed = "#e3350d";
+
+// Use: Background color or for less important buttons and headers. It gives the app a fresh, airy feel.
+const skyBlue = "#3b4cca";
+
+// Use: Secondary buttons, success indicators, or any nature-related features.
+const grassGreen = "#78c850";
+
+// Use: Text color or for dark mode backgrounds, giving contrast to the bright colors.
+const charcoalBlack = "#2d2d2d";
+
+// Use: Background for card elements or secondary sections to create visual separation.
+const lightGray = "#f4f4f4";
+
+// Use: As a complementary color for hover states and links, adding a touch of energy.
+const electricBlue = "#00bfff";
+
+// Use: For softer UI elements, such as badges or labels, to balance the brighter colors.
+const fairyPink = "#ee99ac";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#ECEDEE",
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
   },
 };
